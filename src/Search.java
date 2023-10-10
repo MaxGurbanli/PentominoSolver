764b12e3101b2cb06b143c7f69f79576ef17ecc6
Max Gurbanli <gurbanlimaksud@gmail.com>
2023-10-10 17:31:47 +0200

    Optimized backtracking algorithm approach
    - Constant pentomino ID retrieval time
    
    - Dead spot detection with early pruning

--- src/Search.java
+++ src/Search.java
@@ -1,26 +1,25 @@
-/**
- * @author Department of Data Science and Knowledge Engineering (DKE)
- * @version 2022.0
- */
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * This class includes the methods to support the search of a solution.
- */
-public class Search
-{
+public class Search {
     public static final int horizontalGridSize = 5;
-    public static final int verticalGridSize = 10;
+    public static final int verticalGridSize = 12;
 
-	// 1 of each pentomino letter
-    public static final char[] input = { 'X', 'I', 'Z', 'T', 'U', 'V', 'W', 'Y', 'L', 'F'};
+    // 1 of each pentomino letter
+    public static final char[] input = {'X', 'I', 'Z', 'T', 'U', 'V', 'W', 'Y', 'L', 'F', 'P', 'N'};
 
-    
+    // Map to get pentomino ID in constant time
+    private static final Map<Character, Integer> pentIDMap = new HashMap<>();
+
+    static {
+        for (int i = 0; i < input.length; i++) {
+            pentIDMap.put(input[i], i);
+        }
+    }
+
     //Static UI class to display the board
     public static UI ui = new UI(horizontalGridSize, verticalGridSize, 50);
 
-	/**
-	 * Helper function which starts a basic search algorithm
-	 */
     public static void search() {
         int[][] field = new int[horizontalGridSize][verticalGridSize];
         for (int i = 0; i < field.length; i++) {
@@ -28,37 +27,64 @@ public class Search
                 field[i][j] = -1;
             }
         }
-        if (backtrackSearch(field, 0, ui)) {
-            ui.setState(field);
+        long startTime = System.currentTimeMillis();
+        boolean solutionFound = backtrackSearch(field, 0, ui);
+        long endTime = System.currentTimeMillis();
+        if (solutionFound) {
             System.out.println("Solution found");
         } else {
             System.out.println("No solution found");
         }
+        System.out.println("Time taken: " + (endTime - startTime) / 1000 + " s");
     }
 
-    private static boolean backtrackSearch(int[][] field, int pentominoIndex, UI ui) {
-        if (pentominoIndex == input.length) {
-            return true;  // All pentominoes have been placed successfully
-        }
+private static boolean backtrackSearch(int[][] field, int pentominoIndex, UI ui) {
+    if (pentominoIndex == input.length) {
+        return true;  // All pentominoes have been placed successfully
+    }
 
-        int pentID = characterToID(input[pentominoIndex]);
-        for (int mutation = 0; mutation < PentominoDatabase.data[pentID].length; mutation++) {
-            int[][] pieceToPlace = PentominoDatabase.data[pentID][mutation];
-            for (int x = 0; x <= horizontalGridSize - pieceToPlace.length; x++) {
-                for (int y = 0; y <= verticalGridSize - pieceToPlace[0].length; y++) {
-                    if (canPlace(field, pieceToPlace, x, y)) {
-                        addPiece(field, pieceToPlace, pentID, x, y);
-                        ui.setState(field);
+    int pentID = pentIDMap.get(input[pentominoIndex]);
+    for (int mutation = 0; mutation < PentominoDatabase.data[pentID].length; mutation++) {
+        int[][] pieceToPlace = PentominoDatabase.data[pentID][mutation];
+        for (int x = 0; x <= horizontalGridSize - pieceToPlace.length; x++) {
+            for (int y = 0; y <= verticalGridSize - pieceToPlace[0].length; y++) {
+                if (canPlace(field, pieceToPlace, x, y)) {
+                    addPiece(field, pieceToPlace, pentID, x, y);
+                    ui.setState(field);
+
+                    // Check for dead spots after placing the pentomino
+                    if (!hasDeadSpot(field)) {
                         if (backtrackSearch(field, pentominoIndex + 1, ui)) {
                             return true;  // Found a solution
                         }
-                        removePiece(field, pieceToPlace, x, y);  // Backtrack
                     }
+
+                    removePiece(field, pieceToPlace, x, y);  // Backtrack
                 }
             }
         }
-        return false;  // Couldn't place this pentomino
     }
+    return false;  // Couldn't place this pentomino
+}
+
+private static boolean hasDeadSpot(int[][] field) {
+    // Check for dead spots in the field.
+    // This is a naive approach that checks for single empty cells surrounded by other cells or boundaries.
+    // More sophisticated checks can be added as needed.
+    for (int i = 0; i < field.length; i++) {
+        for (int j = 0; j < field[i].length; j++) {
+            if (field[i][j] == -1) {
+                if ((i == 0 || field[i - 1][j] != -1) &&
+                    (i == field.length - 1 || field[i + 1][j] != -1) &&
+                    (j == 0 || field[i][j - 1] != -1) &&
+                    (j == field[i].length - 1 || field[i][j + 1] != -1)) {
+                    return true;  // Dead spot found
+                }
+            }
+        }
+    }
+    return false;
+}
 
     private static boolean canPlace(int[][] field, int[][] piece, int x, int y) {
         for (int i = 0; i < piece.length; i++) {
@@ -80,70 +106,18 @@ public class Search
             }
         }
     }
-	
-	/**
-	 * Get as input the character representation of a pentomino and translate it into its corresponding numerical value (ID)
-	 * @param character a character representating a pentomino
-	 * @return	the corresponding ID (numerical value)
-	 */
-    private static int characterToID(char character) {
-    	int pentID = -1; 
-    	if (character == 'X') {
-    		pentID = 0;
-    	} else if (character == 'I') {
-    		pentID = 1;
-    	} else if (character == 'Z') {
-    		pentID = 2;
-    	} else if (character == 'T') {
-    		pentID = 3;
-    	} else if (character == 'U') {
-    		pentID = 4;
-     	} else if (character == 'V') {
-     		pentID = 5;
-     	} else if (character == 'W') {
-     		pentID = 6;
-     	} else if (character == 'Y') {
-     		pentID = 7;
-    	} else if (character == 'L') {
-    		pentID = 8;
-    	} else if (character == 'P') {
-    		pentID = 9;
-    	} else if (character == 'N') {
-    		pentID = 10;
-    	} else if (character == 'F') {
-    		pentID = 11;
-    	} 
-    	return pentID;
-    }
-	
-	/**
-	 * Adds a pentomino to the position on the field (overriding current board at that position)
-	 * @param field a matrix representing the board to be fulfilled with pentominoes
-	 * @param piece a matrix representing the pentomino to be placed in the board
-	 * @param pieceID ID of the relevant pentomino
-	 * @param x x position of the pentomino
-	 * @param y y position of the pentomino
-	 */
-    public static void addPiece(int[][] field, int[][] piece, int pieceID, int x, int y)
-    {
-        for(int i = 0; i < piece.length; i++) // loop over x position of pentomino
-        {
-            for (int j = 0; j < piece[i].length; j++) // loop over y position of pentomino
-            {
-                if (piece[i][j] == 1)
-                {
-                    // Add the ID of the pentomino to the board if the pentomino occupies this square
+
+    public static void addPiece(int[][] field, int[][] piece, int pieceID, int x, int y) {
+        for (int i = 0; i < piece.length; i++) {
+            for (int j = 0; j < piece[i].length; j++) {
+                if (piece[i][j] == 1) {
                     field[x + i][y + j] = pieceID;
                 }
             }
         }
     }
 
-	/**
-	 * Main function. Needs to be executed to start the basic search algorithm
-	 */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         search();
     }
 }
