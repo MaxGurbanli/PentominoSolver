60e60161e341eff9c109c07202e6a8bacc0e5a4c
David Wicker <davidwickerhf@gmail.com>
2023-10-12 18:59:27 +0200

    Add testing TODOs

--- src/AlgorithmTesting.java
+++ src/AlgorithmTesting.java
@@ -1,3 +1,12 @@
 public class AlgorithmTesting {
 
+    // Testing parameters
+
+    // Test basic search
+
+    // Test brute force algorithm
+
+    // Test BacktrackingOptimized
+
+    // Test BacktrackingOptimized with FloodFill optimization
 }
